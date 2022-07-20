552a99ab45ebbc111af5ec2f79e137cefbb6c6ab
Laura Walters <laura.walters@clpt.co.uk>
2022-07-20 09:24:21 +0100

    mutations, queries and helper files added

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,27 @@
+import { gql } from "@apollo/client";
+
+// TODO - add authentication token here
+
+// sign up user
+export const ADD_USER = gql`
+	mutation addUser($username: String!, $email: String!, $password: String!) {
+		addUser(username: $username, email: $email, password: $password) {
+			token
+			user {
+				username
+				email
+			}
+		}
+	}
+`;
+
+export const LOGIN_USER = gql`
+	mutation loginUser($email: String!, $password: String!) {
+		loginUser(email: $email, password: $password) {
+			token
+			user {
+				username
+			}
+		}
+	}
+`;
