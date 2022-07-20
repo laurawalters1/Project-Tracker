d97f34d738592318f0e9bf32fe72d102a18112a0
Laura Walters <laura.walters@clpt.co.uk>
2022-07-20 09:22:22 +0100

    frontend auth file added

---
+++ client/src/App.jsx
@@ -0,0 +1,67 @@
+import logo from './logo.svg';
+import './App.css';
+import Auth from "./utils/auth";
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+// router
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  HashRouter,
+} from "react-router-dom";
+
+// http link
+const httpLink = createHttpLink({
+ uri: "http://localhost:3001/graphql" ,
+});
+
+// auth link
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      // we are setting the header on every network request that we make to have the auth token that is available
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+  // new apollo client
+  const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+  });
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
